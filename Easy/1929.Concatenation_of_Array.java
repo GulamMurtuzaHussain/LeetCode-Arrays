092b67ceeed988824903504b7951b80a5b267930
GulamMurtuzaHussain <gulamamir872@gmail.com>
2025-04-20 15:41:13 +0530

    1929.Concatenation_of_Array.java
    In this problem, we have to return an array answer, where answer[i] = nums[i] and answer[i+n] = nums[i]. In simple words, we just need to add the same array twice in the return array.

--- Easy/1929.Concatenation_of_Array.java
+++ Easy/1929.Concatenation_of_Array.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] getConcatenation(int[] nums) {
         int length = nums.length;
         int[] array = new int[2*length];
-
+ 
         int j=0;
         for(int i=0;i<length;i++){
             array[j++] = nums[i];

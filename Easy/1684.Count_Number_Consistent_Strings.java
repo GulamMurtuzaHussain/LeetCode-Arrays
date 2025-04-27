c1970c7a48e34467630b389aff117b438b4171dd
GulamMurtuzaHussain <gulamamir872@gmail.com>
2025-04-28 01:14:57 +0530

    Create 1684.Count_Number_Consistent_Strings.java
    In this problem, we are given a string 'allowed' and a array of strings  and we need to find the total number of consistent strings. The consistent string is the one that only contains the characters available in 'allowed'.

---
+++ Easy/1684.Count_Number_Consistent_Strings.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int consists(String allowed, String word){
+        int contains = 0;
+        for(int i=0;i<word.length();i++){
+            int flag = 0;
+            for(int j=0;j<allowed.length();j++){
+                if(word.charAt(i) == allowed.charAt(j) ){
+                    flag++; break;
+                }
+            }
+            if(flag == 1) contains++;
+        }
+        if(contains == word.length()) return 1;
+        else return 0;
+    }
+    public int countConsistentStrings(String allowed, String[] words) {
+        int consistent = 0;
+        for(int i=0;i<words.length;i++){
+            if(consists(allowed, words[i])==1) consistent++;
+        }
+        return consistent;
+    }
+}
+
+/*
+class Solution {
+    // This function checks if a word only contains allowed characters
+    public boolean isConsistent(String allowed, String word) {
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            // check if current character is in allowed
+            if (allowed.indexOf(ch) == -1) {
+                // character not found in allowed, so it's not consistent
+                return false;
+            }
+        }
+        return true; // all characters were allowed
+    }
+
+    // Main function to count consistent strings
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (isConsistent(allowed, words[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+*/

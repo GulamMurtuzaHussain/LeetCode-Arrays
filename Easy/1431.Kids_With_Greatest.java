a0f1651ae2130fd292320683c5a8fbdfdc2aea8c
GulamMurtuzaHussain <gulamamir872@gmail.com>
2025-05-30 05:58:06 +0530

    1431.Kids_With_Greatest.java (JAVA Code)

--- Easy/1431.Kids_With_Greatest.java
+++ Easy/1431.Kids_With_Greatest.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = 0;
-        for(int i=0;i<candies.length;i++){
+        for(int i=0;i<candies.length;i++){  
             if(candies[i] > max) max = candies[i];
         }
         List<Boolean> result = new ArrayList<>();

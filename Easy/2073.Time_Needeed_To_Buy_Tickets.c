c9751e327ee2013038f049944217818eb5c077fd
GulamMurtuzaHussain <gulamamir872@gmail.com>
2025-06-05 02:20:29 +0530

    2073.Time_Needeed_To_Buy_Tickets.c (C Code)
    There are n people in a line queuing to buy tickets, where the 0th person is at the front of the line and the (n - 1)th person is at the back of the line.
    
    You are given a 0-indexed integer array tickets of length n where the number of tickets that the ith person would like to buy is tickets[i].
    
    Each person takes exactly 1 second to buy a ticket. A person can only buy 1 ticket at a time and has to go back to the end of the line (which happens instantaneously) in order to buy more tickets. If a person does not have any tickets left to buy, the person will leave the line.
    
    Return the time taken for the person initially at position k (0-indexed) to finish buying tickets.

---
+++ Easy/2073.Time_Needeed_To_Buy_Tickets.c
@@ -0,0 +1,23 @@
+int timeRequiredToBuy(int *tickets, int ticketsSize, int k)
+{
+    int time=0;
+    while(tickets[k]>0)
+    {
+        for(int i=0; i<ticketsSize; i++)
+        {
+            if(tickets[k]==0)
+            {
+                return time;
+            }
+            if(tickets[i]<=0){
+                continue;
+            }
+            else
+            {
+                tickets[i]--;
+                time++;
+            }
+        }
+    }
+    return time;
+}

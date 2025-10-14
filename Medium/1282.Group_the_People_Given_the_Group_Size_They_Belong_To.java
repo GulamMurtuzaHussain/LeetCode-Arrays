dc0530302ec2556da8db19793b2163b42f12f09a
GulamMurtuzaHussain <gulamamir872@gmail.com>
2025-10-15 02:06:51 +0530

    1282.Group_the_People_Given_the_Group_Size_They_Belong_To.java
    There are n people that are split into some unknown number of groups. Each person is labeled with a unique ID from 0 to n - 1.
    
    You are given an integer array groupSizes, where groupSizes[i] is the size of the group that person i is in. For example, if groupSizes[1] = 3, then person 1 must be in a group of size 3.
    
    Return a list of groups such that each person i is in a group of size groupSizes[i].
    
    Each person should appear in exactly one group, and every person must be in a group. If there are multiple answers, return any of them. It is guaranteed that there will be at least one valid solution for the given input.

---
+++ Medium/1282.Group_the_People_Given_the_Group_Size_They_Belong_To.java
@@ -0,0 +1,87 @@
+import java.util.Arrays;
+
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        // TC : O(n)
+        
+        HashMap<Integer,ArrayList<Integer>> people = new HashMap<>();
+
+        for(int i=0;i<groupSizes.length;i++){
+            if(!people.containsKey(groupSizes[i])){
+                people.put(groupSizes[i],new ArrayList<Integer>());
+                people.get(groupSizes[i]).add(i);
+            }
+            else people.get(groupSizes[i]).add(i);
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int key : people.keySet()){
+            List<Integer> value_list = people.get(key);
+            List<Integer> temp = new ArrayList<>();
+            for(int i=0;i<value_list.size();i++){
+                if(temp.size() == key){
+                    ans.add(new ArrayList<>(temp));
+                    temp.clear();
+                    temp.add(value_list.get(i));
+                }
+                else{
+                    temp.add(value_list.get(i));
+                }
+            }
+            if(temp.size() !=0){
+                ans.add(new ArrayList<>(temp));
+            }
+        }
+        return ans;
+    }
+}
+
+       /* List<List<Integer>> people = new ArrayList<>(); // OR new ArrayList<List<Integer>>();
+        List<Integer> temp = new ArrayList<>();
+        int[] visited = new int[groupSizes.length];
+        for(int i=0;i<groupSizes.length;i++){
+            if(visited[i] != 0) continue;
+            int size = groupSizes[i];
+            int temp_size = 0;
+            for(int j=i;j<groupSizes.length;j++){
+                if(temp_size == size) break;
+                if(groupSizes[j] == size){
+                    temp.add(j);
+                    temp_size++;
+                    visited[j] = 1;
+                }
+            }
+            people.add(new ArrayList<>(temp));
+            temp.clear();
+        }
+        return people; */
+
+
+/* GPT
+
+   List<List<Integer>> people = new ArrayList<>();
+        boolean[] visited = new boolean[groupSizes.length];
+
+        for (int i = 0; i < groupSizes.length; i++) {
+            if (visited[i]) continue;
+
+            List<Integer> temp = new ArrayList<>();
+            temp.add(i);
+            visited[i] = true;
+            int size = 1;
+
+            for (int j = i + 1; j < groupSizes.length && size < groupSizes[i]; j++) {
+                if (!visited[j] && groupSizes[j] == groupSizes[i]) {
+                    temp.add(j);
+                    visited[j] = true;
+                    size++;
+                }
+            }
+
+            people.add(temp);
+        }
+
+        return people;
+
+    *?
+    */
